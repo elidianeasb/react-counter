6af6bc2906a0530e823e0611a5b1836f7c15542d
Elidiane Bezerra <elidiane.asb@gmail.com>
2023-09-15 20:40:09 +0100

    add event to change theme color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 import Counter from './components/Counter';
+import { useState } from 'react'
 
 function App() {
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = event => {
+    setTheme(event.target.value);
+  };
+
   return (
-    <div className="App">
+    <div className={'App ' + theme}>
+      <div className='theme'>
+        <select onChange={ toggleTheme }>
+          <option value="light"> Light </option>
+          <option value="dark"> Dark </option>
+        </select>
+      </div>
       <Counter/>
     </div>
   );
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react'
-
-
+import './Counter.css';
 
 function Counter() {
     const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+      };
+    
+      const decrement = () => {
+        if (count > 0) {
+          setCount(count - 1);
+        }}
+
+    
     return (
         <div className='counter'>
-            <p>You clicked { count } times</p>
-            <button onClick={() => setCount(count - 1)}> - </button>
-            <button onClick={() => setCount(count + 1)}> + </button>
+            <h2>You clicked { count } times</h2>
+            <button onClick={decrement} disabled={count === 0}>
+                Decrement
+            </button>
+            <button onClick={increment}>Increment</button>
         </div>
   )
 }
